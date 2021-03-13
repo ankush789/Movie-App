e1dcbf07bcc04cbff898e357a1eaecf5fcff271a
ankush789 <ankushsharma.4771@gmail.com>
2021-03-13 21:40:16 +0530

    Subscribing to the store

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,42 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-function App() {
-  return (
-    <div className="App">
-     <Navbar />
-     <div className="main">
-     <div className="tabs">
-       <div className="tab">Movies</div>
-       <div className="tab">Favourites</div>
-     </div>
 
-     <div className="list">
-        {data.map((movie, index) =>
-          <MovieCard movie={movie} key={`movie-${index}`} />
-        )}
-     </div>
-     </div>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount(){
+    const {store} = this.props;
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      this.forceUpdate();
+    })
+    //MAKE API CALLS
+    //Dispatch action
+     store.dispatch({
+      type:'ADD_MOVIES',
+      movies: data
+    });
+    console.log('STATE', this.props.store.getState());
+  }
+  render(){
+      const movies = this.props.store.getState();
+      console.log('Render');
+      return (
+        <div className="App">
+        <Navbar />
+        <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="list">
+            {movies.map((movie, index) =>
+              <MovieCard movie={movie} key={`movie-${index}`} />
+            )}
+        </div>
+        </div>
+        </div>
+      );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,18 +9,18 @@ import movies from './reducers';
 //Creating Store using Redux
 const store = createStore(movies);
 console.log('store', store);
-console.log('Before STATE',store.getState());
+// console.log('Before STATE',store.getState());
 
-//Sending actions to the reducers using dispatch
-store.dispatch({
-  type: 'ADD_MOVIES',
-  movies: [{name:'superman'}]
-});
-console.log('After STate', store.getState());
+// //Sending actions to the reducers using dispatch
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{name:'superman'}]
+// });
+// console.log('After STate', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store = {store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
