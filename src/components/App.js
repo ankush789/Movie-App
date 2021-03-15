c224c12d2f22f720507dec2402ed17277b1a961e
ankush789 <ankushsharma.4771@gmail.com>
2021-03-15 23:51:15 +0530

    Changing State

--- src/components/App.js
+++ src/components/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
     console.log('STATE', this.props.store.getState());
   }
   render(){
-      const movies = this.props.store.getState();
-      console.log('Render');
+      const {list} = this.props.store.getState();// {list:[], favorites:[]}
+      console.log('Render',this.props.store.getState());
       return (
         <div className="App">
         <Navbar />
@@ -29,7 +29,7 @@ class App extends React.Component {
         </div>
 
         <div className="list">
-            {movies.map((movie, index) =>
+            {list.map((movie, index) =>
               <MovieCard movie={movie} key={`movie-${index}`} />
             )}
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,16 @@
 import {ADD_MOVIES} from '../actions';
-export default function movies( state= [], action){
+
+const initialMovieState = {
+    list: [],
+    favorites: []
+}
+
+export default function movies( state = initialMovieState, action){
     if(action.type === ADD_MOVIES){
-        return action.movies
+        return {
+            list: action.movies,
+            favorites: []
+        }
     }
     return state;
 }
